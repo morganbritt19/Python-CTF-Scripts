c2e4321dae9cf73d17541b1db6280e63579633f3
Morgan Britt <60797871+morganbritt19@users.noreply.github.com>
2023-11-28 13:03:30 -0600

    Create JWT Token Decoder.py

---
+++ JWT Token Decoder.py
@@ -0,0 +1,33 @@
+# This uses the PyJWT library to try and decode JSON Web Tokens
+# This was written because decoding it manually using something like CyberChef became frustrating, so I wanted a script that did it for me. 
+# Syntax: python 'JWT Token Decoder.py' <token>
+
+import jwt
+import sys
+
+def decode_jwt(token):
+    try:
+        # Decode the JWT token without verifying the signature
+        decoded_data = jwt.decode(token, algorithms=['HS256'], verify=False)
+        
+        # Print the decoded information
+        print("Decoded JWT Data:")
+        for key, value in decoded_data.items():
+            print(f"{key}: {value}")
+        
+    except jwt.ExpiredSignatureError:
+        print("Error: JWT Signature has expired.")
+    except jwt.InvalidTokenError:
+        print("Error: Invalid JWT token.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python jwt_decoder.py <jwt_token>")
+        sys.exit(1)
+
+    jwt_token = sys.argv[1]
+
+    # Call the function to decode and analyze the JWT
+    decode_jwt(jwt_token)
